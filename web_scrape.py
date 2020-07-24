500a37b74663aac7de05752b796d4af608e67c54
Rupal Singh <rupalsingh.dev@gmail.com>
2020-07-24 22:03:25 +0100

    chore: test selenium heroku 8

--- web_scrape.py
+++ web_scrape.py
@@ -10,7 +10,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 
-options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM", None)
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN", None)
 driver = webdriver.Chrome(chrome_options=options)
 
 # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
