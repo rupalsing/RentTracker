05afb87c7de9966a06427c870056b73df9bc73f5
Rupal Singh <rupalsingh.dev@gmail.com>
2020-07-24 21:58:38 +0100

    chore: test selenium heroku 7

--- web_scrape.py
+++ web_scrape.py
@@ -7,6 +7,8 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
 
 options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM", None)
 driver = webdriver.Chrome(chrome_options=options)
