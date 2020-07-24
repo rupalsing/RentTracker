dc754cbd64ee4b25aaf05ca6106efe5836ca224e
Rupal Singh <rupalsingh.dev@gmail.com>
2020-07-24 21:54:03 +0100

    chore: test selenium heroku 4

--- web_scrape.py
+++ web_scrape.py
@@ -9,7 +9,7 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 
 options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM", None)
-driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options)
 
 # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
 
